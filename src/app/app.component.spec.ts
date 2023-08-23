37e1e27b2bcebc82da3e4ff6cc89e6b80b33f6c1
Stid Rodriguez <stidr.b06@gmail.com>
2023-08-23 14:21:23 -0500

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,17 +11,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'pipes-project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pipes-project');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pipes-project app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes-project';
+
+  name: string = 'Rodríguez';
+  array: number[] = [1,2,3,4,5,6,7,8,9]; 
+  PI: number = Math.PI;
+  percentage: number = 0.3423;
+  salary: number = 1234.5;
+
+  car = {
+    type: '4 puertas pequeño',
+    cc: 1600,
+    motor: '1.4 litros',
+    color: 'negro',
+    brand: 'nissan'
+  }
 }
